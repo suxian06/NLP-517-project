f05d411e7c50b2daf2385cc8bb2b3e8e0155c74a
Su Xian <sxian@nlpg01.cs.washington.edu>
2022-03-14 21:58:51 -0700

    added preprocess

--- train_glue_sst.sh
+++ train_glue_sst.sh
@@ -4,9 +4,9 @@ CUDA_VISIBLE_DEVICES=1 python /homes/iws/sxian/autoencoder/autobot/run_glue.py \
   --model_name_or_path /homes/iws/sxian/autoencoder/salmon/processed/ \
   --task_name SST-2 \
   --data_dir /homes/iws/sxian/autoencoder/autobot/sbert/SST-2/ \
-  --output_dir /homes/iws/sxian/autoencoder/salmon/sentence_classification/sst2_6/ \
+  --output_dir /homes/iws/sxian/autoencoder/salmon/sentence_classification/sst2/ \
   --overwrite_output_dir \
-  --num_train_epochs 6 \
+  --num_train_epochs 10 \
   --seed 42 \
   --learning_rate  2e-5 \
   --weight_decay 0.1 \
